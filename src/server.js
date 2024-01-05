72b56b1ab9c5445b6ef3c12913d682c2a83746c4
Ganeshan Ruwan Kumara <ruwan.ganeshan@acentura.com>
2024-01-05 14:01:34 +0530

    Upvoting articles

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 import express from 'express';
 
+let articlesInfo = [{
+    name:'learn-react',
+    upvotes:0,
+},{
+    name:'learn-js',
+    upvotes:0,
+},{
+    name:'learn-ts',
+    upvotes:0,
+}
+]
+
 const app = express ();
 app.use(express.json());
 
-app.post('/hello',(req,res)=>{
-    res.send(`Hello ${req.body.name}!` );
-})
+app.put('/api/articles/:name/upvote',(req,res) => {
+const { name } = req.params;
+const article = articlesInfo.find(a=> a.name === name)
+if (article){
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`)
+} else {
+    res.send('That article doesnt\'t exist')
+}
 
-app.get('/hello/:name/goodbye/:otherName',(req,res)=>{
-    console.log(req.params);
-    const { name } = req.params;
-    res.send(`Hello ${name}!!`);
 })
 
 app.listen(8000, () => {
