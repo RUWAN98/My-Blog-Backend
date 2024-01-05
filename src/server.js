5f07fa3be3e03d6c7963005e9060a0deb1a64ce2
Ganeshan Ruwan Kumara <ruwan.ganeshan@acentura.com>
2024-01-05 12:37:28 +0530

    URL parameters in Express

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ app.post('/hello',(req,res)=>{
     res.send(`Hello ${req.body.name}!` );
 })
 
+app.get('/hello/:name/goodbye/:otherName',(req,res)=>{
+    console.log(req.params);
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
+})
+
 app.listen(8000, () => {
     console.log ('server is listeni port 8000')
 })
